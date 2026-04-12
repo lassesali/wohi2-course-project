293e09dd89b638901624112ea9f5cee8e170baf7
lassesali <lkarhune@uef.fi>
2026-04-12 23:41:37 +0300

    Add routes

---
+++ src/routes/questions.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const router = express.Router();
+
+const questions = require("../data/questions");
+
+// GET /questions
+// List all questions
+router.get("/", (req, res) => {
+  const { keyword } = req.query;
+
+  if (!keyword) {
+    return res.json(questions);
+  }
+
+  const filteredQuestions = questions.filter(question =>
+    question.keywords.includes(keyword.toLowerCase())
+  );
+
+  res.json(filteredQuestions);
+});
+
+// GET /questions/:questionId
+// Show a specific question
+router.get("/:questionId", (req, res) => {
+  const questionId = Number(req.params.questionId);
+
+   const question = questions.find((q) => q.id === questionId);
+
+  if (!question) {
+    return res.status(404).json({ message: "Question not found" });
+  }
+
+  res.json(question);
+});
+
+//POST /api/questions
+router.post("/", (req,res) => {
+    const { question, answer }  = req.body;
+    if (!question || !answer) {
+        return res.status(400).json({msg: "Question and answer are required"})
+    }
+    
+    const existingIds = questions.map(q => q.id);
+    const maxId = Math.max(...existingIds);
+
+    const newQuestion = {
+        id: questions.length ? maxId + 1 : 1,
+        question, answer
+    };    
+    questions.push(newQuestion);
+    res.status(201).json(newQuestion);
+});
+
+//PUT
+router.put("/:questionId", (req, res) => {
+  const questionId = Number(req.params.questionId);
+  const quest = questions.find((q) => q.id === questionId);
+
+  if (!quest) {
+    return res.status(404).json({ message: "Question not found" });
+  }
+
+  const {question, answer } = req.body;
+    if (!question || !answer) {
+        return res.status(400).json({msg: "Question and answer are required"})
+    }
+
+  quest.question = question;
+  quest.answer = answer;
+  
+  res.json(quest);
+});
+
+//DELETE
+router.delete("/:questionId", (req, res) => {
+  const questionId = Number(req.params.questionId);
+  const questionIndex = questions.findIndex(q => q.id === questionId);
+
+  if(questionIndex === -1){
+    return res.status(404).json({msg:"Question not found"})
+  }
+  const deletedQuestion = questions.splice(questionIndex, 1);
+  res.json({
+    msg: "Question deleted successfully", 
+    question: deletedQuestion
+  });
+});
+
+module.exports = router;
